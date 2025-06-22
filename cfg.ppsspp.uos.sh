785b3c7b1062f9c44ed7204dcf3d7039e4ef31bf
Zhang Jingzhi <30406783+aurantiaco-sucus@users.noreply.github.com>
2025-06-22 10:37:13 +0800

    amend for version 1
    compiled ppsspp for uos (libretro)
    remarks for ppsspp on kylin

--- cfg.ppsspp.uos.sh
+++ cfg.ppsspp.uos.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 cmake -B build -G Ninja . \
 -DCMAKE_BUILD_TYPE=Release \
--DLIBRETRO=1 
+-DLIBRETRO=1 \
+-DCMAKE_C_COMPILER=clang \
+-DCMAKE_CXX_COMPILER=clang++ \
+-DCMAKE_LINKER=ld.lld \
+-DUSE_FFMPEG=OFF \
+-DCMAKE_C_FLAGS="-mcpu=cortex-a75 -fPIC" \
+-DCMAKE_C_FLAGS="-mcpu=cortex-a75 -fPIC"
